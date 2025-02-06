f9ea9ce2e513e32e22474cc71a37e515b8bcc4d7
AbdulRehmanMajid <abdulrehmanmajid2006@gmail.com>
2025-02-06 21:12:09 +0500

    Update Calc.cpp
    Expanded Functionality by adding Multiplication and Division.

--- Calc.cpp
+++ Calc.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Basicfunc
 {
 	private:
-		float sum,sub_sum;
+		float sum,sub_sum,mp_sum=1,div_sum;
 		int size;
 	public:
 		void Addition()
@@ -100,6 +100,94 @@ class Basicfunc
 				
 				
 		}
+		void Multiplication ()
+		{
+			sleep(1);
+			cout<<"How many number do you want to Multiply? || Value must be greater then 1";
+			cout<<endl;
+			cin>>size;
+			system("CLS");
+			cout<<size / 1<<endl;
+			if(size > 1)
+			{
+				
+				float array[size];
+				for(int i = 0; i<size;i++)
+				{
+					system("CLS");
+					cout<<"Enter Number "<<i+1<<endl;
+					float num;
+					
+					cin>>num;
+					
+					
+					array[i] = num;
+				}
+				for(int i = 0; i<size;i++)
+				{
+					system("CLS");
+					mp_sum *= array[i];
+				}
+				cout<<"Total Product of all numbers is: "<<mp_sum<<endl;
+			}
+			else
+			{
+				
+				system("CLS");
+				cout<<"Please enter a value greater then 1"<<endl;
+				sleep(1);
+				
+			Multiplication();
+			}
+				
+				
+		}
+		void Division ()
+		{
+			sleep(1);
+			cout<<"How many number do you want to Divide? || Value must be greater then 1";
+			cout<<endl;
+			cin>>size;
+			system("CLS");
+			cout<<size / 1<<endl;
+			
+			if(size > 1)
+			{
+				
+				float array[size];
+				for(int i = 0; i<size;i++)
+				{
+					system("CLS");
+					cout<<"Enter Number "<<i+1<<endl;
+					float num;
+					
+					cin>>num;
+					
+					
+					array[i] = num;
+					
+				}
+				div_sum = array[0];
+				for(int i = 0; i<size-1;i++)
+				{
+					system("CLS");
+					
+					div_sum /= array[i];
+				}
+				cout<<"Total Dividend of all numbers is: "<<div_sum<<endl;
+			}
+			else
+			{
+				
+				system("CLS");
+				cout<<"Please enter a value greater then 1"<<endl;
+				sleep(1);
+				
+			Division();
+			}
+				
+				
+		}
 		
 };
 void Choice_func()
@@ -109,17 +197,31 @@ void Choice_func()
 	cout<<"Please select one of following:"<<endl;
 	cout<<"1 For Addition"<<endl;
 	cout<<"2 For Subtraction"<<endl;
+	cout<<"3 For Multiplication"<<endl;
+	cout<<"4 For Division"<<endl;
 	
 	
 	cin>>choice;
 	if(choice == 1)
 	{
+		system("CLS");
 		basic_obj.Addition();
 		
 	}
 	else if(choice == 2)
 	{
+		system("CLS");
 		basic_obj.Subtraction();
+	}
+	else if(choice == 3)
+	{
+		system("CLS");
+			basic_obj.Multiplication();
+	}
+	else if(choice == 4)
+	{
+		system("CLS");
+			basic_obj.Division();
 	}
 	else
 	{
@@ -137,4 +239,3 @@ int main()
 	
 	return 0;
 }
-
