5edb046276527316a766a12e7d5cd0b8404354ea
Umair Shamsher <umairshamsher123@gmail.com>
2025-02-07 15:01:32 +0500

    Update Calc.cpp
    Refactored Code.

--- Calc.cpp
+++ Calc.cpp
@@ -1,241 +1,150 @@
 #include <iostream>
 #include <cmath>
-#include <conio.h>
-#include <unistd.h>
+#include <unistd.h>  
 
 using namespace std;
-class Basicfunc
-{
-	private:
-		float sum,sub_sum,mp_sum=1,div_sum;
-		int size;
-	public:
-		void Addition()
-		{
-			sleep(1);
-			cout<<"How many number do you want to add? || Value must be greater then 1";
-			cout<<endl;
-			cin>>size;
-			system("CLS");
-			cout<<size / 1<<endl;
-			if(size > 1)
-			{
-				
-				float array[size];
-				for(int i = 0; i<size;i++)
-				{
-					system("CLS");
-					cout<<"Enter Number "<<i+1<<endl;
-					float num;
-					
-					cin>>num;
-					
-					
-					array[i] = num;
-				}
-				for(int i = 0; i<size;i++)
-				{
-					system("CLS");
-					sum += array[i];
-				}
-				cout<<"Total Sum of all numbers is: "<<sum<<endl;
-			}
-			else
-			{
-				
-				system("CLS");
-				cout<<"Please enter a value greater then 1"<<endl;
-				sleep(1);
-				
-				Addition();
-			}
-				
-				
-		}
-		void Subtraction ()
-		{
-			cout<<"Enter Initial Value"<<endl;
-			float initial;
-			cin>>initial;
-			sleep(1);
-			cout<<"How many number do you want to Subtract? || Value must be greater then 1";
-			cout<<endl;
-			cin>>size;
-			system("CLS");
-			cout<<size / 1<<endl;
-			sub_sum = initial;
-			if(size > 1)
-			{
-				
-				float array[size];
-				for(int i = 0; i<size;i++)
-				{
-					system("CLS");
-					cout<<"Enter Number "<<i+1<<endl;
-					float num;
-					
-					cin>>num;
-					
-					
-					array[i] = num;
-				}
-				
-				for(int i = 0; i<size;i++)
-				{
-					system("CLS");
-					cout<<array[i]<<endl;
-					sub_sum -= array[i];
-				}
-				cout<<"Total Difference of all numbers is: "<<sub_sum<<endl;
-			}
-			else
-			{
-				
-				system("CLS");
-				cout<<"Please enter a value greater then 1"<<endl;
-				sleep(1);
-				
-				Subtraction();
-			}
-				
-				
-		}
-		void Multiplication ()
-		{
-			sleep(1);
-			cout<<"How many number do you want to Multiply? || Value must be greater then 1";
-			cout<<endl;
-			cin>>size;
-			system("CLS");
-			cout<<size / 1<<endl;
-			if(size > 1)
-			{
-				
-				float array[size];
-				for(int i = 0; i<size;i++)
-				{
-					system("CLS");
-					cout<<"Enter Number "<<i+1<<endl;
-					float num;
-					
-					cin>>num;
-					
-					
-					array[i] = num;
-				}
-				for(int i = 0; i<size;i++)
-				{
-					system("CLS");
-					mp_sum *= array[i];
-				}
-				cout<<"Total Product of all numbers is: "<<mp_sum<<endl;
-			}
-			else
-			{
-				
-				system("CLS");
-				cout<<"Please enter a value greater then 1"<<endl;
-				sleep(1);
-				
-			Multiplication();
-			}
-				
-				
-		}
-		void Division ()
-		{
-			sleep(1);
-			cout<<"How many number do you want to Divide? || Value must be greater then 1";
-			cout<<endl;
-			cin>>size;
-			system("CLS");
-			cout<<size / 1<<endl;
-			
-			if(size > 1)
-			{
-				
-				float array[size];
-				for(int i = 0; i<size;i++)
-				{
-					system("CLS");
-					cout<<"Enter Number "<<i+1<<endl;
-					float num;
-					
-					cin>>num;
-					
-					
-					array[i] = num;
-					
-				}
-				div_sum = array[0];
-				for(int i = 0; i<size-1;i++)
-				{
-					system("CLS");
-					
-					div_sum /= array[i];
-				}
-				cout<<"Total Dividend of all numbers is: "<<div_sum<<endl;
-			}
-			else
-			{
-				
-				system("CLS");
-				cout<<"Please enter a value greater then 1"<<endl;
-				sleep(1);
-				
-			Division();
-			}
-				
-				
-		}
-		
+
+class Basicfunc {
+private:
+    float sum = 0, mp_sum = 1, div_sum;
+    int size;
+
+public:
+    void Addition() {
+        sleep(1);
+        cout << "How many numbers do you want to add? || Value must be greater than 1" << endl;
+        cin >> size;
+
+        if (size > 1) {
+            float array[size];
+            sum = 0;  
+            for (int i = 0; i < size; i++) {
+                cout << "Enter Number " << i + 1 << endl;
+                cin >> array[i];
+            }
+
+            for (int i = 0; i < size; i++) {
+                sum += array[i];
+            }
+            cout << "Total Sum of all numbers is: " << sum << endl;
+        } else {
+            cout << "Please enter a value greater than 1" << endl;
+            sleep(1);
+            Addition();  
+        }
+    }
+
+    void Subtraction() {
+        sleep(1);
+        cout << "How many numbers do you want to subtract? || Value must be greater than 1" << endl;
+        cin >> size;
+
+        if (size > 1) {
+            float array[size];
+            sum = 0;  
+            cout << "Enter Number 1" << endl;
+            cin >> sum;  
+            for (int i = 1; i < size; i++) {
+                cout << "Enter Number " << i + 1 << endl;
+                cin >> array[i];
+                sum -= array[i];
+            }
+            cout << "Total Difference of all numbers is: " << sum << endl;
+        } else {
+            cout << "Please enter a value greater than 1" << endl;
+            sleep(1);
+            Subtraction(); 
+        }
+    }
+
+    void Multiplication() {
+        sleep(1);
+        cout << "How many numbers do you want to multiply? || Value must be greater than 1" << endl;
+        cin >> size;
+
+        if (size > 1) {
+            float array[size];
+            mp_sum = 1;  
+            for (int i = 0; i < size; i++) {
+                cout << "Enter Number " << i + 1 << endl;
+                cin >> array[i];
+            }
+
+            for (int i = 0; i < size; i++) {
+                mp_sum *= array[i];
+            }
+            cout << "Total Product of all numbers is: " << mp_sum << endl;
+        } else {
+            cout << "Please enter a value greater than 1" << endl;
+            sleep(1);
+            Multiplication(); 
+        }
+    }
+
+    void Division() {
+        sleep(1);
+        cout << "How many numbers do you want to divide? || Value must be greater than 1" << endl;
+        cin >> size;
+
+        if (size > 1) {
+            float array[size];
+            cout << "Enter Number 1" << endl;
+            cin >> div_sum;  
+            for (int i = 1; i < size; i++) {
+                cout << "Enter Number " << i + 1 << endl;
+                cin >> array[i];
+                if (array[i] == 0) {
+                    cout << "Cannot divide by zero!" << endl;
+                    return;
+                }
+                div_sum /= array[i];  // Apply division.
+            }
+            cout << "Total Dividend of all numbers is: " << div_sum << endl;
+        } else {
+            cout << "Please enter a value greater than 1" << endl;
+            sleep(1);
+            Division();  
+        }
+    }
 };
-void Choice_func()
-{
-	Basicfunc basic_obj;
-	int choice;
-	cout<<"Please select one of following:"<<endl;
-	cout<<"1 For Addition"<<endl;
-	cout<<"2 For Subtraction"<<endl;
-	cout<<"3 For Multiplication"<<endl;
-	cout<<"4 For Division"<<endl;
-	
-	
-	cin>>choice;
-	if(choice == 1)
-	{
-		system("CLS");
-		basic_obj.Addition();
-		
-	}
-	else if(choice == 2)
-	{
-		system("CLS");
-		basic_obj.Subtraction();
-	}
-	else if(choice == 3)
-	{
-		system("CLS");
-			basic_obj.Multiplication();
-	}
-	else if(choice == 4)
-	{
-		system("CLS");
-			basic_obj.Division();
-	}
-	else
-	{
-		system("CLS");
-		cout<<"Wrong Choice!"<<endl;
-		sleep(1);
-		system("CLS");
-		Choice_func();
-	}
+
+void Choice_func() {
+    Basicfunc basic_obj;
+    int choice;
+    cout << "Please select one of the following:" << endl;
+    cout << "1 For Addition" << endl;
+    cout << "2 For Subtraction" << endl;
+    cout << "3 For Multiplication" << endl;
+    cout << "4 For Division" << endl;
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        basic_obj.Addition();
+        cin.get();
+        break;
+    case 2:
+        basic_obj.Subtraction();
+        cin.get();
+        break;
+    case 3:
+        basic_obj.Multiplication();
+        cin.get();
+        break;
+    case 4:
+        basic_obj.Division();
+        cin.get();
+        break;
+    default:
+        cout << "Wrong Choice!" << endl;
+        sleep(1);
+        Choice_func();  
+        break;
+    }
 }
-int main()
-{
-	
-	Choice_func();
-	
-	return 0;
+
+int main() {
+    Choice_func();
+    cin.get();
+    return 0;
 }
